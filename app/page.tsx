7c6ccb39044253f0c69288544dc6c9b7ba8a2277
Lucas-Hardeman <lhh22@fsu.edu>
2025-09-16 21:26:00 -0400

    changed some of the main page

--- app/page.tsx
+++ app/page.tsx
@@ -63,7 +63,7 @@ export default function HomePage() {
       >
         <h2 className="text-2xl font-bold mb-4">About Me</h2>
         <p className="text-gray-400 leading-relaxed">
-          I’m recently finished my{" "}
+          I recently finished my{" "}
           <span className="text-gray-200">
             B.S. in Computer Science at Florida State University
           </span>{" "}
