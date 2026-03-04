71db492ca9a09ac3917beba74807cc007b7831d7
Lucas Hardeman <lhh22@fsu.edu>
2026-03-04 12:11:22 -0500

    Update page.tsx

--- app/nb/page.tsx
+++ app/nb/page.tsx
@@ -10,7 +10,17 @@ export default function NB() {
     <div>
       <h1>Notebook</h1>
       <pre>
-{`2026-02-26
+{`
+
+2026-03-04
+- Changed topic to OpenPLC
+- Looked for papers on acm site
+
+2026-02-27
+- Started working within the VM 
+- Made a small syscall program that did work
+
+2026-02-26
 - Start the Fuzzer
 - Work
 
